5dfedcbb3ca380f2bc81b2be0e3e4de2e6fa92fa
Roman Marinichev <rmarinicev@gmail.com>
2022-11-12 14:28:40 +0300

    access

--- docker/pgsql/init.sql
+++ docker/pgsql/init.sql
@@ -1,7 +1,49 @@
+create table roles
+(
+    id SERIAL not null constraint PK_Role primary key,
+    name varchar (30) not null
 
-CREATE TABLE users (
-  id integer NOT NULL,
-  login text NOT NULL UNIQUE,
-  password text NOT NULL,
-  PRIMARY KEY (id)
 );
+
+create table tokens
+(
+    id SERIAL not null constraint PK_Token primary key,
+    status bool not null,
+    expired_date timestamp not null,
+    token varchar (255) not null
+);
+
+create table users
+(
+    id SERIAL not null constraint PK_User primary key,
+    phone varchar (30) not null,
+    password varchar (200) not null,
+    role_id int not null references roles (id),
+    token_id int references tokens (id)
+);
+
+create table payments
+(
+    id SERIAL not null constraint PK_Payment primary key,
+    amount integer not null,
+    user_id int not null references users (id),
+    bill_id varchar (200) not null,
+    status varchar (30) not null,
+    pay_token_identifier   varchar (255) not null
+);
+
+create or replace procedure roles_Insert (p_name varchar(30))
+language plpgsql
+as $$
+ DECLARE have_record int := count(*) from roles where name = p_name;
+	begin
+	if have_record > 0 then
+			raise exception 'Уже есть данная запись';
+	else
+insert into roles(name)
+values (p_name);
+	end if;
+	end;
+$$;
+
+insert into roles(name) values ('user'),('merchant');
